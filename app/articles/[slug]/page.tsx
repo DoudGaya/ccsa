1523bee3cefa04eca3347bd7181a3e40975bfa10
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-01-20 17:00:29 +0100

    items error

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,35 +1,71 @@
-import { SanityTypes } from '@/@types'
-import { DynamicBanner } from '@/app/components/dynamics/DynamicBanner'
-import { getSingleArticle } from '@/sanity/lib/queries'
-import React from 'react'
-import ArticleContents from './_components/ArticleContents'
+import type { SanityTypes } from "@/@types"
+import { DynamicBanner } from "@/app/components/dynamics/DynamicBanner"
+import { getSingleArticle } from "@/sanity/lib/queries"
+import type React from "react"
+import ArticleContents from "./_components/ArticleContents"
 
-const Page = async ({ params }: {
+interface PageProps {
   params: {
     slug: string
   }
-}) => {
-  const { slug } = params 
+}
+
+const Page: React.FC<PageProps> = async ({ params }) => {
+  const { slug } = params
 
   try {
-    // @ts-ignore
-    const article = await getSingleArticle(slug) as SanityTypes.Article
+    const article = (await getSingleArticle(slug)) as SanityTypes.Article
     return (
-      <div className='flex flex-col w-full'>
-        <DynamicBanner 
-          title={article.title} 
-          bannerImage={article.imageUrl} 
-          description={article.overview} 
-        />
-       <div className=" py-6">
+      <div className="flex flex-col w-full">
+        <DynamicBanner title={article.title} bannerImage={article.imageUrl} description={article.overview} />
+        <div className="py-6">
           <ArticleContents article={article} />
-       </div>
+        </div>
       </div>
     )
   } catch (error) {
-    console.error('Error fetching article:', error)
+    console.error("Error fetching article:", error)
     throw error // Let Next.js error boundary handle this
   }
 }
 
-export default Page  // Capitalize component name per React conventions
+export default Page
+
+
+
+
+// import { SanityTypes } from '@/@types'
+// import { DynamicBanner } from '@/app/components/dynamics/DynamicBanner'
+// import { getSingleArticle } from '@/sanity/lib/queries'
+// import React from 'react'
+// import ArticleContents from './_components/ArticleContents'
+
+// const Page = async ({ params }: {
+//   params: {
+//     slug: string
+//   }
+// }) => {
+//   const { slug } = params 
+
+//   try {
+//     // @ts-ignore
+//     const article = await getSingleArticle(slug) as SanityTypes.Article
+//     return (
+//       <div className='flex flex-col w-full'>
+//         <DynamicBanner 
+//           title={article.title} 
+//           bannerImage={article.imageUrl} 
+//           description={article.overview} 
+//         />
+//        <div className=" py-6">
+//           <ArticleContents article={article} />
+//        </div>
+//       </div>
+//     )
+//   } catch (error) {
+//     console.error('Error fetching article:', error)
+//     throw error // Let Next.js error boundary handle this
+//   }
+// }
+
+// export default Page  // Capitalize component name per React conventions
