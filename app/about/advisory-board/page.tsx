9ee3b48579a8e0edc00d25e0c4790a02f9835173
DoudGaya <arbdoule@yahoo.com>
2025-01-29 11:16:30 +0100

    some advisory board

--- app/about/advisory-board/page.tsx
+++ app/about/advisory-board/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+// import { AboutBanner } from '../_component/AboutBanner'
+import { AboutBanner } from '../_component/AboutBanner'
+import AboutBannerImage from '@/public/about-banner.jpg'
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+    <AboutBanner 
+        title="Advisory Board" 
+        description=''
+        bannerImage={AboutBannerImage.src} />
+    </div>
   )
 }
 
