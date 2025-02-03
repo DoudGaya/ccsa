6c1957d2573592da6373de53c9d04a2075f49a48
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-02-03 10:46:39 +0100

    merge

--- app/activities/_components/ActivitiesBanner.tsx
+++ app/activities/_components/ActivitiesBanner.tsx
@@ -18,7 +18,7 @@ export const ActivitiesBanner  = ({
         backgroundPosition: 'center',
         backgroundSize: 'cover',
     }}
-    className='h-[calc(100vh-30vh)] w-full flex bg-gradient-to-b bg-black/50 bg-top bg-blend-overlay relative bg-cover bg-center bg-no-repeat text-blue-50 bg-fixed'>
+    className='h-[calc(100vh-30vh)] w-full flex bg-gradient-to-b bg-black/50 bg-top bg-blend-overlay relative bg-cover  bg-no-repeat text-blue-50 bg-fixed'>
     <div className="bg-gradient-to-b flex-col from-transparent justify-center  w-full flex ">
        <div className=" max-w-7xl space-y-3 text-start mx-auto pb-6 md:pb-10 px-8 pt-20">
        <h1 className=' text-2xl md:text-4xl xl:text-5xl font-bold font-blog'>{title}</h1>
