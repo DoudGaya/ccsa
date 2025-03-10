16a6a5db9042554e1108c7256902f72129366804
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-03-10 17:29:06 +0100

    ethics

--- app/activities/_components/AcitivityContents.tsx
+++ app/activities/_components/AcitivityContents.tsx
@@ -11,22 +11,6 @@ const ActivityContents = ( {
 }) => {
   return (
     <div className='flex flex-col max-w-6xl w-full space-y-6 mx-auto px-8'>
-       {/* <Link href={`/staffs/${activity.author.slug}`} className="flex space-x-3 cursor-pointer hover:bg-gray-200 max-w-max px-3 py-1 rounded-xl items-center  ">
-        {
-            activity.author.imageUrl && (
-                <Image src={activity.author.imageUrl} alt={activity.title} className=' rounded-full h-[50px] w-[50px] object-cover' width={300} height={300} />
-            )
-        }
-        <div className="  flex flex-col text-start items-start">
-            <p className=' text-xs font-poppins'> {activity.author.designation}</p>
-            <p className=' font-blog '> {activity.author.name}</p>
-
-        </div>
-    </Link> */}
-
-      
-
-
        <article className=" prose lg:prose-lg dark:prose-invert font-blog text-lg space-y-6">
         <PortableText 
         // @ts-ignore
