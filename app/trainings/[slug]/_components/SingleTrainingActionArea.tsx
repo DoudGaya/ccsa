ed2d3386cd81e865f98d0896519569052027f200
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-04-12 08:44:44 +0100

    typo fix

--- app/trainings/[slug]/_components/SingleTrainingActionArea.tsx
+++ app/trainings/[slug]/_components/SingleTrainingActionArea.tsx
@@ -33,7 +33,7 @@ export function SingleTrainingActionArea({
   return (
         <Dialog>
             <DialogTrigger asChild>
-            <Button className='bg-green-600 hover:bg-green-700 text-white font-medium py-4 h-full px-6 rounded-md transition duration-300'>Apply for a Taraining</Button>
+            <Button className='bg-green-600 hover:bg-green-700 text-white font-medium py-4 h-full px-6 rounded-md transition duration-300'>Apply for a Training</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[700px] bg-white dark:bg-black max-h-[80%] md:max-w-xl overflow-y-auto">
             <DialogHeader>
