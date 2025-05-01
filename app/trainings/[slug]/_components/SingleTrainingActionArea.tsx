92e2a60c3a66f8f7cc06354960c44186939dee31
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-05-01 21:28:11 +0100

    images

--- app/trainings/[slug]/_components/SingleTrainingActionArea.tsx
+++ app/trainings/[slug]/_components/SingleTrainingActionArea.tsx
@@ -21,7 +21,6 @@ export function SingleTrainingActionArea({
 }: {
     training: SanityTypes.Trainings
 }) {
-  // const [TrainingItem, setTrainingItem] = useState<(SanityTypes.Trainings)>(training)
   const [isDialogOpen, setIsDialogOpen] = useState(false)
 
     const HandleHomeAdd = () => {
@@ -34,7 +33,7 @@ export function SingleTrainingActionArea({
         <Dialog>
             <DialogTrigger asChild>
             <Button className='bg-green-600 hover:bg-green-700 text-white font-medium py-4 h-full px-6 rounded-md transition duration-300'>Apply for a Training</Button>
-            </DialogTrigger>
+            </DialogTrigger> 
             <DialogContent className="sm:max-w-[700px] bg-white dark:bg-black max-h-[80%] md:max-w-xl overflow-y-auto">
             <DialogHeader>
                 <DialogTitle className='py-5 flex text-center bg-green-200 dark:text-green-200 rounded-lg justify-center'>
